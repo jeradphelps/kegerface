853dbeac740c1e93f2a503312e2a63f769e1947a
Jerad Phelps <jerad.phelps@gmail.com>
2014-02-22 08:51:36 -0600

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kegerface::Application.config.secret_key_base = '66ec980ddfe8a6e6afe11622c6428cf738e8765a9793441511521b991725d5da33eff48a9065a30690e214aa52a0f8a1d573fc7c8f891fdd3c638e1af0dd66af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kegerface::Application.config.session_store :cookie_store, key: '_kegerface_session'
