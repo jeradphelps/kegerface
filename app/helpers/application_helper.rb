73b02544aee1ad15c5de48c01b70504e3876ddee
Jerad Phelps <jerad.phelps@gmail.com>
2014-03-11 22:16:38 -0500

    display images based on numbers from db

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,36 @@ module ApplicationHelper
   def nice_date date_time
     date_time.strftime("%m/%d/%Y")
   end
+
+
+
+
+
+
+
+
+  # Image Helpers
+  def get_srm_image beer
+    [40,36,30,25,20,15,10,5,1].each do |srm_image_nbr|
+      if beer.srm.to_i >= srm_image_nbr
+        return image_tag "SRM #{srm_image_nbr}.png", :height => "150"
+      end
+    end
+  end
+
+  def get_hop_image beer
+    [75,50,25,0].each do |hop_image_nbr|
+      if beer.ibu.to_i >= hop_image_nbr
+        return image_tag "#{hop_image_nbr} Hops.png", :width => "200"
+      end
+    end
+  end
+
+  def get_status_image beer
+    [100,75,50,25,10,0].each do |status_image_nbr|
+      if beer.status.to_i >= status_image_nbr
+        return image_tag "kegs/#{status_image_nbr}.png", :height => "150"
+      end
+    end
+  end
 end
